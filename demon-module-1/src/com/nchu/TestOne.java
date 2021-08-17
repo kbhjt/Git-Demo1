339becc782bd157c7409c93009be51acc7529b1e
kbhjt <2411817976@qq.com>
2021-08-17 16:43:14 +0800

    第三次修改

--- demon-module-1/src/com/nchu/TestOne.java
+++ demon-module-1/src/com/nchu/TestOne.java
@@ -3,5 +3,6 @@ package com.nchu;
 public class TestOne {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("test change");
     }
 }
